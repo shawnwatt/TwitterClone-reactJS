d7c9373e30d795099979f89a791ae3cefb8eb41a
shawnwatt <mrwatt68@gmail.com>
2020-09-23 11:10:37 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Feed from "./Feed";
 import Sidebar from "./Sidebar";
+import Widgets from "./Widgets";
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
       <Feed />
+      <Widgets />
       {/* sidebar */}
       {/* feed */}
       {/* Widgets */}
